f47b9694f2bb975715012545bb3b61bcd3027f86
Florian Miller <flokomi@web.de>
2023-02-28 15:11:49 +0100

    show and create working, waiting for current user to proceed

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,10 +1,12 @@
 class ReservationsController < ApplicationController
   def create
     @reservation = Reservation.new(reservation_params)
-    @user = User.last
-    @reservation.user = @user
+    @reservation.user = User.last # MUST BE CURRENT USER
     @spaceship = Spaceship.find(params[:spaceship_id])
     @reservation.spaceship = @spaceship
+
+    set_end_price_in_reservation
+
     if @reservation.save
       redirect_to spaceship_path(@reservation)
     else
@@ -16,5 +18,12 @@ class ReservationsController < ApplicationController
 
   def reservation_params
     params.require(:reservation).permit(:start_date, :end_date, :total_guests, :price)
+  end
+
+  def set_end_price_in_reservation
+    end_of = Date.parse(params[:reservation][:end_date])
+    start_of = Date.parse(params[:reservation][:start_date])
+    num_of_days = (end_of - start_of).to_i
+    @reservation.price = num_of_days * @spaceship.price
   end
 end
