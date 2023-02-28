9259d1c7ff1e1eeec47bd429a79532bcf909233e
Felix Cramer <felixcramer18@gmail.com>
2023-02-28 14:14:11 +0100

    checking initial seed function

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+User.destroy_all
+Spaceship.destroy_all
+
+puts 'Creating 3 fake users...'
+3.times do
+  user = User.new(
+    name: Faker::Name.name.to_s,
+    email: Faker::Internet.email.to_s,
+    password: Faker::Internet.password.to_s
+  )
+  user.save!
+end
+puts "The final frontier..."
+
+puts 'Creating 8 fake spaceships...'
+
+8.times do
+  spaceship = Spaceship.new(
+    name: Faker::Space.nasa_space_craft.to_s,
+    price: rand(150..100_000),
+    capacity: rand(1..50),
+    user_id: rand(1..3)
+  )
+  spaceship.save!
+end
+
+puts "Blast off!" 
+
+puts "#{Spaceship.count} spaceships were created. #{User.count} users were created."
